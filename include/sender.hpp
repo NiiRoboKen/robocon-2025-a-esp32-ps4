a0b6b77a009dadb261955a332e6c4024875da433
Sootta <soutaaibara@gmail.com>
2025-10-06 01:19:58 +0900

    feat/side arm

--- include/sender.hpp
+++ include/sender.hpp
@@ -20,5 +20,34 @@ namespace send
             sendSBTP(data, 4);
         }
     }
-
+    namespace roger {
+        void move(uint8_t duty, bool is_up) {
+            uint8_t up = is_up ? 1 : 0;
+            uint8_t data[3] = {0x20, duty, up};
+            sendSBTP(data, 3);
+        } 
+    }
+    namespace arm {
+        void right(uint8_t is_open, uint8_t is_open_move, uint8_t is_fold, uint8_t is_fold_move) {
+            uint8_t data[5] = {0x31, is_open, is_open_move, is_fold, is_fold_move};
+            sendSBTP(data, 5);
+        }
+        void left(uint8_t is_open, uint8_t is_open_move, uint8_t is_fold, uint8_t is_fold_move) {
+            uint8_t data[5] = {0x32, is_open, is_open_move, is_fold, is_fold_move};
+            sendSBTP(data, 5);
+        }
+    }
+    namespace arm_up
+    {
+        void moveRight(uint8_t duty, bool is_up) {
+            uint8_t up = is_up ? 1 : 0;
+            uint8_t data[3] = {0x41, duty, up};
+            sendSBTP(data, 3);
+        } 
+        void moveLeft(uint8_t duty, bool is_up) {
+            uint8_t up = is_up ? 1 : 0;
+            uint8_t data[3] = {0x42, duty, up};
+            sendSBTP(data, 3);
+        } 
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,74 @@
 #include "sbtp.hpp"
 #include "sender.hpp"
 
+//UART1 PIN
 const int8_t SERIAL1_RX_PIN = 4;
 const int8_t SERIAL1_TX_PIN = 5;
 
+enum class State {
+  NotConnected,
+  ControlSteering,
+  ControlSideArm,
+  ControlFrontArm
+};
+
+State state = State::NotConnected;
+
 void control_str();
+void controlSideArm();
 
 void setup() {
   Serial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, SERIAL1_RX_PIN, SERIAL1_TX_PIN);
-  //PS4.begin("00:02:5b:00:a5:ac");
   PS4.begin("e4:65:b8:7e:12:1c");
 }
 
 void loop() {
-  if(!PS4.isConnected()) return;
-  control_str();
+    if(!PS4.isConnected()) {
+    state = State::NotConnected;
+    return;
+  }
+  
+  //ホームボタンでモード切り替え
+  static bool is_home_btn = 1;
+  static uint8_t state_number = 1;
+
+  if(PS4.PSButton() && !is_home_btn) {
+    is_home_btn = true;
+    state_number++;
+    if(state_number >= 4) state_number = 1;
+    state = State(state_number);
+  }
+  else if(!PS4.PSButton() && is_home_btn) {
+    is_home_btn = false;
+  }
+
+  switch (state){
+    case State::ControlSteering:
+      //独ステ制御
+      static float rad = 0;
+      PS4.setLed(255,0,255);
+      PS4.sendToController();
+      control_str();
+      break;
+
+    case State::ControlSideArm:
+      //アーム制御
+      PS4.setLed(0,255,255);
+      PS4.sendToController();
+      controlSideArm();
+      break;
+
+    case State::ControlFrontArm:
+      //好一さんのアーム
+      PS4.setLed(255,0,0);
+      PS4.sendToController();
+      //controlFrontArm();
+      break;
+
+    default:
+      break;
+  }
   delay(50);
 }
 
@@ -41,4 +94,100 @@ void control_str(){
   else if(sqrt(x * x + y * y) > 125) {
     send::steering::rotate_duty(duty_str, (int16_t)stick_deg, state);
   }
+}
+
+void controlSideArm() {
+  //ロジャー機構
+  static bool is_roger_btn = false;
+  if(PS4.R2Value() > 15 || PS4.L2Value() > 15){
+    is_roger_btn = true;
+    if(PS4.R2Value() > 15) {
+      uint8_t duty = (double)(PS4.R2Value() - 10) * 100 / 255;
+      send::roger::move(duty, true);
+    }
+    else {
+      uint8_t duty = (double)(PS4.L2Value() - 10) * 100 / 255;
+      send::roger::move(duty, false);
+    }
+  }
+  else if(!(PS4.R2Value() > 15 || PS4.L2Value() > 15) && is_roger_btn) {
+    send::roger::move(0, false);
+    is_roger_btn = false;
+  }
+
+  //右アーム昇降
+  static bool is_right_elevator_btn = false;
+  if(abs(PS4.RStickY()) > 25){
+    is_right_elevator_btn = true;
+    bool is_up = PS4.RStickY() > 0;
+    uint8_t duty = (double)(abs(PS4.RStickY()) - 20) * 100. / 127.;
+    send::arm_up::moveRight(duty, is_up);
+  }
+  else if(abs(PS4.RStickY()) < 50 && is_right_elevator_btn){
+    is_right_elevator_btn = false;
+    send::arm_up::moveRight(0, false);
+  }
+
+  //左アーム昇降
+  static bool is_left_elevator_btn = false;
+  if(abs(PS4.LStickY()) > 25){
+    is_left_elevator_btn = true;
+    bool is_up = PS4.LStickY() > 0;
+    uint8_t duty = (double)(abs(PS4.LStickY()) - 20) * 100. / 127.;
+    send::arm_up::moveLeft(duty, is_up);
+  }
+  else if(abs(PS4.LStickY()) < 50 && is_left_elevator_btn){
+    is_left_elevator_btn = false;
+    send::arm_up::moveLeft(0, false);
+  }
+  
+  //右アーム動作
+  //展開
+  static bool is_right_arm_open = false;
+  if((PS4.Right() || PS4.Left()) && !is_right_arm_open) {
+    is_right_arm_open = true;
+    uint8_t open = PS4.Right() ? 1 : 0;
+    send::arm::right(open, 1, 0, 0);
+  }
+  else if(!(PS4.Left() || PS4.Right()) && is_right_arm_open) {
+    is_right_arm_open = false;
+    send::arm::right(0, 0, 0, 0);
+  }
+  //掴む
+  static bool is_right_arm_fold = false;
+  if((PS4.Up() || PS4.Down()) && !is_right_arm_fold) {
+    is_right_arm_fold = true;
+    uint8_t up = PS4.Up() ? 1 : 0;
+    send::arm::right(0,0,up,1);
+  }
+  else if(!(PS4.Up() || PS4.Down()) && is_right_arm_fold) {
+    is_right_arm_fold = false;
+    send::arm::right(0,0,0,0);
+  }
+
+  //左アーム動作
+  //展開
+  static bool is_left_arm_open = false;
+  if((PS4.Circle() || PS4.Square()) && !is_left_arm_open) {
+    is_left_arm_open = true;
+    uint8_t open = PS4.Circle() ? 1 : 0;
+    send::arm::right(open, 1, 0, 0);
+  }
+  else if(!(PS4.Circle() || PS4.Square()) && is_left_arm_open) {
+    is_left_arm_open = false;
+    send::arm::left(0, 0, 0, 0);
+  }
+  //掴む
+  static bool is_left_arm_fold = false;
+  if((PS4.Triangle() || PS4.Cross()) && !is_left_arm_fold) {
+    is_left_arm_fold = true;
+    uint8_t up = PS4.Triangle() ? 1 : 0;
+    send::arm::left(0,0,up,1);
+  }
+  else if(!(PS4.Triangle() || PS4.Cross()) && is_left_arm_fold) {
+    is_left_arm_fold = false;
+    send::arm::left(0,0,0,0);
+  }
+
+
 }
